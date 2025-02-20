3b1ad7e5eaf181b34cab37a1f58d6825a419f039
Matt Pero <mattpero22@gmail.com>
2025-02-20 11:25:25 -0500

    working example of model viewer loading a model that is stored locally

--- cam_viewer.py
+++ cam_viewer.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('EXAMPLE.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
